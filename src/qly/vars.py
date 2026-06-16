ae6bd6757d2e057aae5fa69bfd888927c8b40743
Georg <gk_coder@icloud.com>
2026-06-16 21:32:43 +0200

    feat: shared variables

---
+++ src/qly/vars.py
@@ -0,0 +1,8 @@
+# vars.py - shared storage between qly Processes
+# Licensed under the Apache-2.0 or BSD 3-Clause license
+
+commands = ["val", "ln"]
+ra = [4, 2] 
+types = ["int", "bool"]
+vars = []
+var_content = []
